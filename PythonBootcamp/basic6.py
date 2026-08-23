c132d639ffc0f39a6ffd5324fab5a9c58072b5b4
Almash Lakdawala <almash.lkd@outlook.com>
2026-08-23 11:42:48 +0530

    basic Python learning 2 from gfg

--- PythonBootcamp/basic6.py
+++ PythonBootcamp/basic6.py
@@ -12,3 +12,20 @@ if s == n:
     print("Armstrong Number")
 else:
     print("Not an Armstrong Number")
+
+    n = 153
+    p = len(str(n))
+
+
+
+
+    def arm(x):
+        if x == 0:
+            return 0
+        return (x % 10) ** p + arm(x // 10)
+
+
+    if arm(n) == n:
+        print("Armstrong Number")
+    else:
+        print("Not an Armstrong Number")
