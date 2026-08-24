954feef8a4805d5f1db584869c7829cb08c39f73
Almash Lakdawala <almash.lkd@outlook.com>
2026-08-24 19:37:02 +0530

    basic Python learning 2 from gfg

---
+++ PythonBootcamp/test1.py
@@ -0,0 +1,26 @@
+age = 18
+
+if age >= 18 :
+    print("not eligible")
+
+    num  = 4
+
+    if num % 2 == 0 :
+        print("the number is even ")
+    else:
+        print("the number is odd")
+
+        score = 85
+
+        if score >= 90:
+            grade = "A"
+        elif score >= 80:
+            grade = "B"
+        elif score >= 70:
+            grade = "C"
+        elif score >= 60:
+            grade = "D"
+        else:
+            grade = "F"
+
+        print("Your grade is:", grade)
