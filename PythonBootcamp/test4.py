97f0f0a343dd2c3fe7eadb62a13815222f8bb415
Almash Lakdawala <almash.lkd@outlook.com>
2026-08-25 23:04:02 +0530

    basic Python learning 2 from gfg

---
+++ PythonBootcamp/test4.py
@@ -0,0 +1,17 @@
+num = 40
+
+for i in range(0,num):
+    print(i)
+
+
+
+cnt = 0
+while (cnt < 3):
+        cnt = cnt + 1
+        print("Hello Geek")
+
+
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end=' ')
+    print()
