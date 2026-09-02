188e92c0afe8244a1dcc65d9e7d448f1d09bb4b4
Almash Lakdawala <almash.lkd@outlook.com>
2026-09-02 23:49:52 +0530

    basic Python learning 2 from gfg

--- PythonBootcamp/day1.py
+++ PythonBootcamp/day1.py
@@ -1,10 +1,28 @@
-n = 5
-res = ((n * (n + 1)) // 2) ** 2
-print(res)
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
 
-c = 'A'
-print(ord(c))
+  def move(self):
+    print("Move!")
 
-arr = [10, 324, 45, 90, 9808]
-res = max(arr)
-print(res)
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
+
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
