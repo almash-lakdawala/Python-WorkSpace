f8267993acf441796e0a7760d07ce9d85d12147a
Almash Lakdawala <almash.lkd@outlook.com>
2026-09-03 23:04:24 +0530

    basic Python learning 2 from gfg

--- PythonBootcamp/basic4.py
+++ PythonBootcamp/basic4.py
@@ -1,16 +1,38 @@
-def fun(p, t, r):
-    return (p * t * r) / 100
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
 
-p, t, r = 8, 6, 8
+# Insert method to create nodes
+   def insert(self, data):
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         elif data > self.data:
+            if self.right is None:
+               self.right = Node(data)
+            else:
+               self.right.insert(data)
+      else:
+         self.data = data
 
-res = fun(p, t, r)
-print(res)
+# Print the tree
+   def Postorder(self):
+      if self.left:
+         self.left.Postorder()
+      if self.right:
+         self.right.Postorder()
+      print(self.data, "->", end = "")
 
-
-
-si = lambda p, t, r: (p * t * r) / 100
-
-p, t, r = 8, 6, 8
-
-res = si(p, t, r)
-print(res)
+root = Node(54)
+root.insert(34)
+root.insert(46)
+root.insert(12)
+root.insert(23)
+root.insert(5)
+print("Postorder Traversal: ")
+root.Postorder()
