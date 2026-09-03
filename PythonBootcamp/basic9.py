1222404849a96c95343c62cf5ccd44940f921a7f
Almash Lakdawala <almash.lkd@outlook.com>
2026-09-03 23:04:15 +0530

    basic Python learning 2 from gfg

--- PythonBootcamp/basic9.py
+++ PythonBootcamp/basic9.py
@@ -1,6 +1,38 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n-1) + fibonacci(n-2)
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
 
-print(fibonacci(9))
+# Insert method to create nodes
+   def insert(self, data):
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         elif data > self.data:
+            if self.right is None:
+               self.right = Node(data)
+            else:
+               self.right.insert(data)
+      else:
+         self.data = data
+
+# Print the tree
+   def Postorder(self):
+      if self.left:
+         self.left.Postorder()
+      if self.right:
+         self.right.Postorder()
+      print(self.data, "->", end = "")
+
+root = Node(54)
+root.insert(34)
+root.insert(46)
+root.insert(12)
+root.insert(23)
+root.insert(5)
+print("Postorder Traversal: ")
+root.Postorder()
